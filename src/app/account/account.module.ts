dd9b24087c16ef097704eff9170a86fa577647f4
Hồ Thái Khanh <hothaikhanh@gmail.com>
2023-06-07 01:10:14 +0700

    initial commit

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AccountRoutingModule } from './account-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent,
+  ],
+  imports: [
+    CommonModule,
+    AccountRoutingModule
+  ]
+})
+export class AccountModule { }
